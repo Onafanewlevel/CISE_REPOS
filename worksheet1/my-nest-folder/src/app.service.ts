706ebd5572f6fc9c4261f6560aacf37fab76c384
Onafanewlevel <rosemicluvin@icloud.com>
2024-08-19 04:08:55 +1200

    added text to getHello()

--- worksheet1/my-nest-folder/src/app.service.ts
+++ worksheet1/my-nest-folder/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'This is testing the \'Developent\' branch';
+    return 'This is Q.10 of worksheet 1 showing the creation and running of the \'NEST\' application';
   }
 }
